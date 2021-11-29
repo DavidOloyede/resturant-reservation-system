6e77c8a77d5d7cafc6da9216905cf99942b5b547
SuddsJE <55158728+SuddsJE@users.noreply.github.com>
2021-11-28 21:13:49 -0600

    Revert "updates"
    This reverts commit 423191132a23f52678adfae33b83cc765682465e.

--- server/index.js
+++ server/index.js
@@ -2,92 +2,40 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
-const cors= require("cors");
-const e = require('express');
+const cors = require('cors');
 
-/*
 const db = mysql.createPool({
     host: 'localhost', //change necessary information 
     user: 'root',
     password: 'password',
     database: 'cruddatabase',
 });
-*/
-
-
-const db = mysql.createPool({
-    host: 'localhost', //change necessary information 
-    port: '3306',
-    user: 'root',
-    password: 'password',
-    database: '4351project',
-});
 
 //The following bulk code was used to check if the connection to the server was working. Use as test if needed.
-
 /*
 app.get("/", (req, res) =>{
-    const sqlInsert = "INSERT INTO users (email, password) VALUES ('inception', 'good movie');"
+    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('inception', 'good movie');"
     db.query(sqlInsert, (err, result) => {
+        console.log(err);
         res.send("hello Jaime, bong bong");
     });
 });
 */
 
-app.use(express.json())
 app.use(cors());
-app.post('/register', (req,res)=>{
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-    const email=req.body.email
-    const firstName= req.body.firstName
-    const lastName= req.body.lastName
-    const password=req.body.password
+app.post('/api/insert', (req, res)=> {
 
-    db.query("INSERT INTO users (email, firstName, lastName, password) VALUES (?,?,?,?)",
-    [email, firstName, lastName, password], 
-    (err, result)=>{
-        console.log(err);
-        }
-    );
+    const movieName = req.body.movieName;
+    const movieReview = req.body.movieReview;
+
+    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?)";
+    db.query(sqlInsert, [movieName, movieReview], (err, result)=>{
+        console.log(result);
+    });
 });
-
-app.post('/login', (req,res)=>{
-    const email=req.body.email
-    const password=req.body.password
-
-    db.query("SELECT * FROM users WHERE email = ? AND password = ?",
-    [email, password], 
-    (err, result)=>{
-        if(err){
-            res.send({err:err});
-        }
-        if(result.length>0){
-            res.send(result);
-        }else{
-            res.send({message:"Wrong username or password"});
-        }
-        
-        }
-    );
-});
-
-app.post('/reservation', (req,res)=>{
-    const email=req.body.email
-    const name=req.body.name
-    const phone=req.body.phone
-    const date=req.body.date
-    const time=req.body.time
-    const guest=req.body.guest
-
-    db.query("INSERT INTO reservations (email, name, phone, date, time, guest) VALUES (?,?,?,?,?,?)",
-    [email, name, phone, date, time, guest], 
-    (err, result)=>{
-        console.log(err);
-        }
-    );
-});
-
-//app.get("/", (req, res) =>{});
 
 app.listen(3001, () => {
     console.log('running on port 3001');
